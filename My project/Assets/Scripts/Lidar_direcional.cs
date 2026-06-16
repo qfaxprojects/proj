c0af86afca261d5085a2ec5e913ed2041149dcb5
qfaxas <148553536+qfaxas@users.noreply.github.com>
2026-06-16 07:12:31 -0300

    ta indo

--- My project/Assets/Scripts/Lidar_direcional.cs
+++ My project/Assets/Scripts/Lidar_direcional.cs
@@ -14,13 +14,13 @@ public class Lidar_direcional : MonoBehaviour
     private InputAction _fire;
     private InputAction _changeRadius;
     private List<Vector3> _positionsList = new();
+    private List<int> _enemyPositions = new();
     private List<VisualEffect> _vfxList = new();
     private VisualEffect _currentVFX;
     private Texture2D _texture;
     private Color[] _positions;
     private bool _createNewVFX;
     private LineRenderer _lineRenderer;
-    private int _particleAmount = 0;
 
     private const string TEXTURE_NAME = "PositionsTexture";
     private const string RESOLUTION_PARAMETER_NAME = "Resolution";
@@ -29,14 +29,17 @@ public class Lidar_direcional : MonoBehaviour
     [SerializeField] private LayerMask _layerMask;
     [SerializeField] private VisualEffect _vfxPrefab;
     [SerializeField] private GameObject _vfxContainer;
+    [SerializeField] private int _maxContainer = 10;
+
     [SerializeField] private Transform _castPoint;
     [SerializeField] private float _radius = 10f;
     [SerializeField] private float _maxRadius = 10f;
     [SerializeField] private float _minRadius = 1f;
-    [SerializeField] private int _pointsPerScan = 40;
+    
     [SerializeField] private float _range = 10f;
 
     [SerializeField] private int resolution = 100;
+    [SerializeField][Tooltip("Quanto maior, mais objs cria(e mais rapido)")] private int _pointsPerScan = 100;
 
 
     private void OnEnable()
@@ -58,6 +61,7 @@ public class Lidar_direcional : MonoBehaviour
         _lineRenderer.enabled = false;
         _createNewVFX = true;
 
+
         CreateNewVFX();
         ApplyPositions();
     }
@@ -72,33 +76,99 @@ public class Lidar_direcional : MonoBehaviour
     {
         if (_changeRadius.triggered)
         {
-            _radius = Math.Clamp(_radius + _changeRadius.ReadValue<float>() * Time.deltaTime, _minRadius, _maxRadius);
+            _radius = Math.Clamp(_radius + _changeRadius.ReadValue<float>(), _minRadius, _maxRadius);
         }
     }
 
     private void Scan()
     {
+        /* adaptação ponto a ponto(vvai precisar de vvvarios raycasts)
+        int i = 0;
+
         if (_fire.IsPressed())
         {
-            for (int i = 0; i < _pointsPerScan; i++)
+            if (i < _pointsPerScan)
             {
-                Vector3 randomPoint = Random.insideUnitSphere * _radius;
-                randomPoint += _castPoint.position;
+                i++;
+                _lineRenderer.enabled = true;
+
+                Vector3 randomPoint = Random.insideUnitSphere;
+                randomPoint.z = Math.Abs(randomPoint.z);
+
+                randomPoint += _castPoint.position + _castPoint.TransformDirection(randomPoint) * _radius;
+
+
 
                 Vector3 direction = (randomPoint - transform.position).normalized;
 
                 if (Physics.Raycast(transform.position, direction, out RaycastHit hit, _range, _layerMask))
                 {
-                    if (_positionsList.Count < resolution * resolution)
+                    if (_positionsList.Count < resolution * resolution - 1)
                     {
                         _positionsList.Add(hit.point);
                         _lineRenderer.enabled = true;
                         _lineRenderer.SetPositions(new[]
                         {
+                                transform.position,
+                                hit.point
+                            });
+                        _particleAmount++;
+                    }
+                    else
+                    {
+                        _createNewVFX = true;
+                        CreateNewVFX();
+                    }
+                }
+                ApplyPositions();
+            }
+            else
+            {
+                i = 0;
+            }
+        }
+        
+        
+    
+        else 
+        {
+            _lineRenderer.enabled = false;
+        }
+        */
+        
+        if (_fire.IsPressed())
+        {
+            for (int i = 0; i < _pointsPerScan; i++)
+            {
+                Vector3 randomPoint = Random.insideUnitSphere;
+                randomPoint.z = Math.Abs(randomPoint.z);
+
+                randomPoint += _castPoint.position + _castPoint.TransformDirection(randomPoint) * _radius;
+
+
+
+                Vector3 direction = (randomPoint - transform.position).normalized;
+
+                if (Physics.Raycast(transform.position, direction, out RaycastHit hit, _range, _layerMask))
+                {
+                    Debug.Log(_positionsList.Count);
+                    if (_positionsList.Count < resolution * resolution)
+                    {
+                        _positionsList.Add(hit.point);
+                        _lineRenderer.enabled = true;
+
+                        //checa se é um inimigo
+                        if (hit.collider.CompareTag("Enemy"))
+                        {
+                            _enemyPositions.Add(i);
+                        }
+
+
+                        _lineRenderer.SetPositions(new[]
+                        {
                             transform.position,
                             hit.point
                         });
-                        _particleAmount++;
                     }
                     else
                     {
@@ -117,13 +187,29 @@ public class Lidar_direcional : MonoBehaviour
         
         }
         
+
     }
 
     private void CreateNewVFX()
     {
         if (!_createNewVFX) return;
 
-        _vfxList.Add(_currentVFX);
+        if (_vfxList.Count < _maxContainer)
+        {
+            _vfxList.Add(_currentVFX);
+        }
+        else
+        {
+
+            _vfxList.RemoveAt(0);
+            _vfxList.Add(_currentVFX);
+            VisualEffect temp = _vfxContainer.gameObject.GetComponentInChildren<VisualEffect>();
+            Debug.Log(temp.gameObject);
+            Destroy(temp.gameObject);
+
+
+        }
+
 
         _currentVFX = Instantiate(_vfxPrefab, transform.position, Quaternion.identity, _vfxContainer.transform);
         _currentVFX.SetUInt(RESOLUTION_PARAMETER_NAME, (uint)resolution);
@@ -133,8 +219,6 @@ public class Lidar_direcional : MonoBehaviour
         _positions = new Color[resolution * resolution];
 
         _positionsList.Clear();
-
-        _particleAmount = 0;
 
         _createNewVFX = false;
     }
@@ -155,7 +239,9 @@ public class Lidar_direcional : MonoBehaviour
             Color data;
             if (i < posListLen - 1)
             {
+                
                 data = new Color(pos[i].x - vfxPos.x, pos[i].y - vfxPos.y, pos[i].z - vfxPos.z, 1);
+
             }
             else
             {
